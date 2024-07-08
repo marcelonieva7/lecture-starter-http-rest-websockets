c89602a51e79d30c1d1e12a1fe8f20ce844d84a0
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-08 17:53:28 -0300

    refactor: remove useless code

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,15 +3,7 @@ import room from './room.js';
 
 import { Server } from 'socket.io';
 
-import * as config from './config.js';
-
 export default (io: Server) => {
     user(io.of("/game")),
     room(io.of("/game"));
-
-/*     io.on('connection', socket => {
-        const username = socket.handshake.query.username;
-        console.log(`User ${username} connected SERVERRRRRRR`);
-        
-    }); */
 };
