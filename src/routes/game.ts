e5da52a65edbd7f7a0861107a027ef711a002aa3
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-07 23:00:53 -0300

    feat: add route to get text by id

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import { Router } from 'express';
 
 import { HTML_FILES_PATH } from '../config.js';
+import { texts } from '../data.js';
 
 const router = Router();
 
@@ -9,5 +10,15 @@ router.get('/', (req, res) => {
     const page = path.join(HTML_FILES_PATH, 'game.html');
     res.sendFile(page);
 });
+
+router.get('/texts/:id', (req, res) => {
+    const { id } = req.params;
+    const text = texts.at(parseInt(id));
+    if (!text) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json({ text });
+})
 
 export default router;
