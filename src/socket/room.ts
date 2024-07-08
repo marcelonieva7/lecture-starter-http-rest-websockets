1dd2455b588155ec173913f7488bf856eba2cc88
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-08 17:54:23 -0300

    feat: handle ready msg

--- src/socket/room.ts
+++ src/socket/room.ts
@@ -1,23 +1,47 @@
-import { type Namespace } from "socket.io";
-import { type Room } from "./types.js";
-import { MAXIMUM_USERS_FOR_ONE_ROOM } from "./config.js";
+import { type Socket, type Namespace } from "socket.io";
+import { type User, type Room } from "./types.js";
+import { MAXIMUM_USERS_FOR_ONE_ROOM, SECONDS_FOR_GAME, SECONDS_TIMER_BEFORE_START_GAME } from "./config.js";
+import { users } from "./user.js";
+import { texts } from "../data.js";
 
 const roomsMap = new Map<string, Room>();
 const isUniqueRoomName = (roomName: string) => roomsMap.get(roomName) === undefined;
 
-roomsMap.set("lobby", { numberOfUsers: 1, isHidden: false });
+roomsMap.set("lobby", { numberOfUsers: 0, isHidden: false });
 roomsMap.set("game", { numberOfUsers: 22, isHidden: false });
 
-const getCurrentRoomName = socket => {
+const getCurrentRoomName = (socket: Socket) => {
   let currrentRoomName: string | undefined;
   roomsMap.forEach((_room, name) => {
     if (socket.rooms.has(name)) {
       currrentRoomName = name;
     }
-  })
-
+  });
   return currrentRoomName;
 }
+
+const getUsersInRoom = (io: Namespace, roomName: string): User[] => {
+  const room = io.adapter.rooms.get(roomName);
+  if (!room) return [];
+
+  return Array.from(room)
+    .map(socketId => users.get(socketId))
+    .filter((user): user is User => user !== undefined);
+};
+
+const checkIfAllUsersAreReady = (io: Namespace, roomName: string ): void => {
+  const usersInRoom = getUsersInRoom(io, roomName);
+
+  if (usersInRoom.every(user => user.isReady)) {
+    io.to(roomName).emit("START_GAME", {
+      SECONDS_TIMER_BEFORE_START_GAME,
+      SECONDS_FOR_GAME,
+      textId : getRandomTextId(texts)
+    });
+  }
+}
+
+const getRandomTextId = (texts: string[]) => Math.floor(Math.random() * texts.length);
 
 export default (io: Namespace) => {
   io.on("connection", socket => {
@@ -62,6 +86,11 @@ export default (io: Namespace) => {
         roomsMap.set(roomName, updatedRoom);
 
         socket.join(roomName);
+       
+        const usersInRoom = getUsersInRoom(io, roomName);
+        socket.emit("SHOW_USERS_IN_ROOM", usersInRoom);
+        socket.to(roomName).emit("ADD_USER_IN_ROOM", users.get(socket.id));
+        
         io.emit("UPDATE_ROOM", [roomName, updatedRoom]);
 
       } else {
@@ -83,13 +112,39 @@ export default (io: Namespace) => {
             isHidden: false
           };
           roomsMap.set(currentRoomName, updatedRoom);
+
+          socket.to(currentRoomName).emit("DELETE_USER_IN_ROOM", users.get(socket.id));
           io.emit("UPDATE_ROOM", [currentRoomName, updatedRoom]);
+
+          checkIfAllUsersAreReady(io, currentRoomName);
 
         } else {
           roomsMap.delete(currentRoomName);
           io.emit("DELETE_ROOM", currentRoomName);
         }
       }
+    })
+
+    socket.on("READY", () => {
+      const currentUser = users.get(socket.id)!
+      const updatedUser = { ...currentUser, isReady: true };
+
+      users.set(socket.id, updatedUser);
+
+      const room = getCurrentRoomName(socket)!
+      socket.to(room).emit("UPDATE_USERS_STATUS", updatedUser);
+
+      checkIfAllUsersAreReady(io, room);
+    })
+
+    socket.on("NOT_READY", () => {
+      const currentUser = users.get(socket.id)!
+      const updatedUser = { ...currentUser, isReady: false };
+
+      users.set(socket.id, updatedUser);
+
+      const room = getCurrentRoomName(socket)!
+      socket.to(room).emit("UPDATE_USERS_STATUS", updatedUser);
     })
   });
 };
