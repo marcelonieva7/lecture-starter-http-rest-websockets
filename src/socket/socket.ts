6fe6eeaebd536452d937103d5c003e4645de069e
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-07 22:59:04 -0300

    feat: implement sockets

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,9 +1,17 @@
+import user from './user.js';
+import room from './room.js';
+
 import { Server } from 'socket.io';
 
 import * as config from './config.js';
 
 export default (io: Server) => {
-    io.on('connection', socket => {
+    user(io.of("/game")),
+    room(io.of("/game"));
+
+/*     io.on('connection', socket => {
         const username = socket.handshake.query.username;
-    });
+        console.log(`User ${username} connected SERVERRRRRRR`);
+        
+    }); */
 };
