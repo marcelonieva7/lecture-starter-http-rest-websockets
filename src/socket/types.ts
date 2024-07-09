8871965aa14c9b0078e73cd95024bdd4865dc040
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-08 23:21:24 -0300

    feat: add race result to room type

--- src/socket/types.ts
+++ src/socket/types.ts
@@ -1,11 +1,13 @@
 type Room = {
   numberOfUsers: number;
   isHidden: boolean;
+  raceResult: string[];
 };
 
 type User = {
   name: string;
   isReady: boolean;
+  isFinished: boolean;
 }
 
 export { type Room, type User };
